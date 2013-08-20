a93f0b521f9d3a068bea70ce5686202eeafb6157
horst-n <info@nogajski.de>
2013-08-21 01:49:47 +0200

    updated to Version 0.0.5  - added method canvas  - added method unsharpMask
    Signed-off-by: horst-n <info@nogajski.de>

--- ImageManipulator.class.php
+++ ImageManipulator.class.php
@@ -6,7 +6,7 @@
 * @copyright   -  (c) 2002 - 2013
 * -------------------------------------------------------------------------
 * $Source: /WEB/pw2/htdocs/site/modules/PageImageManipulator/ImageManipulator.class.php,v $
-* $Id: ImageManipulator.class.php,v 1.14 2013/08/17 20:11:19 horst Exp $
+* $Id: ImageManipulator.class.php,v 1.18 2013/08/21 01:09:53 horst Exp $
 *********************************************************************************************/
 
 
@@ -1023,6 +1023,150 @@ class ImageManipulator extends Wire {
 		}
 
 
+		/*
+		Unsharp masking is a traditional darkroom technique that has proven very
+		suitable for
+		digital imaging. The principle of unsharp masking is to create a blurred
+		copy of the image
+		and compare it to the underlying original. The difference in colour values
+		between the two images is greatest for the pixels near sharp edges.
+		When this
+		difference is subtracted from the original image, the edges will be
+		accentuated.
+
+		The Amount parameter simply says how much of the effect you want. 100 is 'normal'.
+		Radius is the radius of the blurring circle of the mask. 'Threshold' is the least
+		difference in colour values that is allowed between the original and the mask.
+		In practice
+		this means that low-contrast areas of the picture are left unrendered whereas edges
+		are treated normally. This is good for pictures of e.g. skin or blue skies.
+
+		Any suggenstions for improvement of the algorithm, expecially regarding the speed
+		and the roundoff errors in the Gaussian blur process, are welcome.
+		//////////////////////////////////////////////////////////////////
+		////
+		////                  p h p U n s h a r p M a s k
+		////
+		////    Unsharp mask algorithm by Torstein Hønsi 2003.
+		////             thoensi_at_netcom_dot_no.
+		////               Please leave this notice.
+		////
+		/////////////////////////////////////////////////////////////////*/
+		public function unsharpMask($amount=100, $radius=0.5, $threshold=3) {
+			if($this->bypassOperations) {
+				return $this;
+			}
+			if(false === ($im = $this->imLoad())) {
+				throw new WireException("Cannot load the MemoryImage!");
+				return false;
+			}
+			// Attempt to calibrate the parameters to Photoshop:
+			if($amount > 500)		$amount = 500;
+			$amount = $amount * 0.016;
+			if($radius > 50)		$radius = 50;
+			$radius = $radius * 2;
+			if($threshold > 255)	$threshold = 255;
+
+			$radius = abs(round($radius));	// Only integers make sense.
+			if($radius == 0) {
+				imagedestroy($im);
+				break;
+				return $im;
+			}
+			$w = imagesx($im); $h = imagesy($im);
+			$imgCanvas = imagecreatetruecolor($w, $h);
+			$imgCanvas2 = imagecreatetruecolor($w, $h);
+			$imgBlur = imagecreatetruecolor($w, $h);
+			$imgBlur2 = imagecreatetruecolor($w, $h);
+			imagecopy ($imgCanvas, $im, 0, 0, 0, 0, $w, $h);
+			imagecopy ($imgCanvas2, $im, 0, 0, 0, 0, $w, $h);
+
+			// Gaussian blur matrix:
+			//
+			//    1    2    1
+			//    2    4    2
+			//    1    2    1
+			//
+			//////////////////////////////////////////////////
+
+			// Move copies of the image around one pixel at the time and merge them with weight
+			// according to the matrix. The same matrix is simply repeated for higher radii.
+			for($i = 0; $i < $radius; $i++) {
+				imagecopy ($imgBlur, $imgCanvas, 0, 0, 1, 1, $w - 1, $h - 1); //up left
+				imagecopymerge ($imgBlur, $imgCanvas, 1, 1, 0, 0, $w, $h, 50); //down right
+				imagecopymerge ($imgBlur, $imgCanvas, 0, 1, 1, 0, $w - 1, $h, 33.33333); //down left
+				imagecopymerge ($imgBlur, $imgCanvas, 1, 0, 0, 1, $w, $h - 1, 25); //up right
+				imagecopymerge ($imgBlur, $imgCanvas, 0, 0, 1, 0, $w - 1, $h, 33.33333); //left
+				imagecopymerge ($imgBlur, $imgCanvas, 1, 0, 0, 0, $w, $h, 25); //right
+				imagecopymerge ($imgBlur, $imgCanvas, 0, 0, 0, 1, $w, $h - 1, 20 ); //up
+				imagecopymerge ($imgBlur, $imgCanvas, 0, 1, 0, 0, $w, $h, 16.666667); //down
+				imagecopymerge ($imgBlur, $imgCanvas, 0, 0, 0, 0, $w, $h, 50); //center
+				imagecopy ($imgCanvas, $imgBlur, 0, 0, 0, 0, $w, $h);
+
+				// During the loop above the blurred copy darkens, possibly due to a roundoff
+				// error. Therefore the sharp picture has to go through the same loop to
+				// produce a similar image for comparison. This is not a good thing, as processing
+				// time increases heavily.
+				imagecopy ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h);
+				imagecopymerge ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h, 50);
+				imagecopymerge ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h, 33.33333);
+				imagecopymerge ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h, 25);
+				imagecopymerge ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h, 33.33333);
+				imagecopymerge ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h, 25);
+				imagecopymerge ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h, 20 );
+				imagecopymerge ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h, 16.666667);
+				imagecopymerge ($imgBlur2, $imgCanvas2, 0, 0, 0, 0, $w, $h, 50);
+				imagecopy ($imgCanvas2, $imgBlur2, 0, 0, 0, 0, $w, $h);
+			}
+
+			// Calculate the difference between the blurred pixels and the original
+			// and set the pixels
+			for($x = 0; $x < $w; $x++) { // each row
+				for($y = 0; $y < $h; $y++) { // each pixel
+					$rgbOrig = ImageColorAt($imgCanvas2, $x, $y);
+					$rOrig = (($rgbOrig >> 16) & 0xFF);
+					$gOrig = (($rgbOrig >> 8) & 0xFF);
+					$bOrig = ($rgbOrig & 0xFF);
+
+					$rgbBlur = ImageColorAt($imgCanvas, $x, $y);
+
+					$rBlur = (($rgbBlur >> 16) & 0xFF);
+					$gBlur = (($rgbBlur >> 8) & 0xFF);
+					$bBlur = ($rgbBlur & 0xFF);
+
+					// When the masked pixels differ less from the original
+					// than the threshold specifies, they are set to their original value.
+					$rNew = (abs($rOrig - $rBlur) >= $threshold)
+						? max(0, min(255, ($amount * ($rOrig - $rBlur)) + $rOrig))
+						: $rOrig;
+					$gNew = (abs($gOrig - $gBlur) >= $threshold)
+						? max(0, min(255, ($amount * ($gOrig - $gBlur)) + $gOrig))
+						: $gOrig;
+					$bNew = (abs($bOrig - $bBlur) >= $threshold)
+						? max(0, min(255, ($amount * ($bOrig - $bBlur)) + $bOrig))
+						: $bOrig;
+
+					if (($rOrig != $rNew) || ($gOrig != $gNew) || ($bOrig != $bNew)) {
+							$pixCol = ImageColorAllocate($im, $rNew, $gNew, $bNew);
+							ImageSetPixel($im, $x, $y, $pixCol);
+					}
+				}
+			}
+
+			imagedestroy($imgCanvas);
+			imagedestroy($imgCanvas2);
+			imagedestroy($imgBlur);
+			imagedestroy($imgBlur2);
+			if(!$this->isResourceGd($im)) {
+				throw new WireException("Error when applying filter grayscale to  MemoryImage.");
+				return false;
+			}
+
+			return $this->imWrite($im) ? $this : false;
+		}
+
+
+
 
 	// Filter Methods
 
@@ -1317,14 +1461,17 @@ class ImageManipulator extends Wire {
 				return false;
 			}
 			@imagealphablending($im, true);
-			$sourcefile_width=imageSX($im);
-			$sourcefile_height=imageSY($im);
-			$watermarkfile_width=imageSX($wm);
-			$watermarkfile_height=imageSY($wm);
-			$watermarkfile_width_new=imageSX($wm);
-			$watermarkfile_height_new=imageSY($wm);
+			$sourcefile_width=imagesx($im);
+			$sourcefile_height=imagesy($im);
+			$padding = !is_int($padding) || $padding<0 || $padding>25 ? 2 : $padding;
+			$padding = $sourcefile_width > $sourcefile_height ? intval($sourcefile_height / 100 * $padding) : intval($sourcefile_width / 100 * $padding);
+
+			$watermarkfile_width=imagesx($wm);
+			$watermarkfile_height=imagesy($wm);
+			$watermarkfile_width_new=imagesx($wm);
+			$watermarkfile_height_new=imagesy($wm);
 			$calc = new hn_SizeCalc();
-			$calc->down($watermarkfile_width_new, $watermarkfile_height_new, $sourcefile_width, $sourcefile_height);
+			$calc->down($watermarkfile_width_new, $watermarkfile_height_new, $sourcefile_width-$padding, $sourcefile_height-$padding);
 			unset($calc);
 			$wm2 = $this->createTruecolor($watermarkfile_width_new, $watermarkfile_height_new);
 			@imagealphablending($wm2, false);
@@ -1339,8 +1486,6 @@ class ImageManipulator extends Wire {
 			$posX = intval(($sourcefile_width - $watermarkfile_width_new) / 2);
 			$posY = intval(($sourcefile_height - $watermarkfile_height_new) / 2);
 			if('center'!=$position && 'c'!=$position) {
-				$padding = !is_int($padding) || $padding<0 || $padding>25 ? 2 : $padding;
-				$padding = $sourcefile_width > $sourcefile_height ? intval($sourcefile_height / 100 * $padding) : intval($sourcefile_width / 100 * $padding);
 				switch($position) {
 					case 'n':
 					case 'north':
@@ -1628,10 +1773,118 @@ class ImageManipulator extends Wire {
 		}
 
 
+		public function canvas($width, $height=null, $bgcolor=null, $position=null, $padding=null) {
+			if($this->bypassOperations) {
+				return $this;
+			}
+			if(is_array($width) && null===$height) {
+				// we have an options array as param
+				$options = $width;
+				$valid = array('width', 'height', 'bgcolor', 'position', 'padding');
+				foreach($options as $k=>$v) {
+					if(!in_array($k, $valid)) {
+						continue;
+					}
+					$$k = $v;
+				}
+			}
+			if(null===$bgcolor) {
+				$bgcolor = array(255,255,255);
+			}
+			$bgcolor = $this->sanitizeColor($bgcolor);
+			$position = !is_string($position) || (!in_array(strtolower($position), $this->positioningValues) && !in_array(strtolower($position), array_keys($this->positioningValues))) ? 'center' : strtolower($position);
+			if(!is_int($width) || $width<10 || !is_int($height) || $height<10) {
+				throw new WireException("Wrong params width or height with method canvas!");
+				return false;
+			}
+			if(false === ($im = $this->imLoad())) {
+				throw new WireException("Cannot load the MemoryImage!");
+				return false;
+			}
+			$imWidth = imagesx($im);
+			$imHeight = imagesy($im);
+			$padding = !is_int($padding) || $padding<0 || $padding>25 ? 0 : $padding;
+			$padding = $imWidth > $imHeight ? intval($imHeight / 100 * $padding) : intval($imWidth / 100 * $padding);
+            $imWidthNew = imagesx($im);
+			$imHeightNew = imagesy($im);
+			$calc = new hn_SizeCalc();
+			$calc->down($imWidthNew, $imHeightNew, $width - $padding, $height - $padding);
+			unset($calc);
+            if($imWidthNew!=$imWidth || $imHeightNew!=$imHeight) {
+				$im2 = $this->createTruecolor($imWidthNew, $imHeightNew);
+				if(!@imagecopyresampled($im2, $im, 0, 0, 0, 0, $imWidthNew, $imHeightNew, $imWidth, $imHeight)) {
+					throw new WireException("Error when trying to resize watermarkLogo to fit to the MemoryImage.");
+					return false;
+				}
+				$im = $this->createTruecolor($imWidthNew, $imHeightNew);
+				@imagecopy($im, $im2, 0, 0, 0, 0, $imWidthNew, $imHeightNew);
+				@imagedestroy($im2);
+            }
+            $canvas = $this->createTruecolor($width, $height);
+			@imagealphablending($canvas, true);
+            $bg1 = imagecolorallocate($canvas, 255,255,255);
+            @imagefilledrectangle($canvas, 0, 0, $width, $height, $bg1);
+            $bg = count($bgcolor)==4 ? imagecolorallocatealpha($canvas, $bgcolor[0], $bgcolor[1], $bgcolor[2], $bgcolor[3]) : imagecolorallocate($canvas, $bgcolor[0], $bgcolor[1], $bgcolor[2]);
+            @imagefilledrectangle($canvas, 0, 0, $width, $height, $bg);
+			@imagealphablending($canvas, true);
+			@imagesavealpha($canvas, true);
+			switch($position) {
+				case 'n':
+				case 'north':
+					$posX = intval(($width - $imWidthNew) / 2);
+					$posY = $padding;
+					break;
+				case 's':
+				case 'south':
+					$posX = intval(($width - $imWidthNew) / 2);
+					$posY = intval(($height - $imHeightNew) - $padding);
+					break;
+				case 'w':
+				case 'west':
+					$posX = $padding;
+					$posY = intval(($height - $imHeightNew) / 2);
+					break;
+				case 'e':
+				case 'east':
+					$posX = intval($width - $imWidthNew - $padding);
+					$posY = intval(($height - $imHeightNew) / 2);
+					break;
+				case 'nw':
+				case 'northwest':
+					$posX = $padding;
+					$posY = $padding;
+					break;
+				case 'ne':
+				case 'northeast':
+					$posX = intval($width - $imWidthNew - $padding);
+					$posY = $padding;
+					break;
+				case 'sw':
+				case 'southwest':
+					$posX = $padding;
+					$posY = intval(($height - $imHeightNew) - $padding);
+					break;
+				case 'se':
+				case 'southeast':
+					$posX = intval($width - $imWidthNew - $padding);
+					$posY = intval(($height - $imHeightNew) - $padding);
+					break;
+				default:
+					$posX = intval(($width - $imWidthNew) / 2);
+					$posY = intval(($height - $imHeightNew) / 2);
+			}
+			$res = @imagecopy($canvas, $im, $posX, $posY, 0, 0, $imWidthNew, $imHeightNew);
+			@imagedestroy($im);
+			if(!$res || !$this->isResourceGd($canvas)) {
+				throw new WireException("Error when trying to apply a watermarkLogo to the MemoryImage.");
+				return false;
+			}
+			return $this->imWrite($canvas) ? $this : false;
+		}
 
 
 
-	// static oneLiner Methods that can be called only with a filename passed to them
+	// static oneLiner Methods that can be called with only a filename passed to them
 
 		public static function fileGetExifOrientation($filename, $return_correctionArray=false) {
 			// first value is rotation-degree and second value is flip-mode: 0=NONE | 1=HORIZONTAL | 2=VERTICAL
@@ -1787,7 +2040,15 @@ class ImageManipulator extends Wire {
 		}
 
 		private function sanitizeColor($value) {
-			if( is_array($value) && count($value)==3 ) {
+			if( is_array($value) && count($value)==4 ) {
+				$tmp = array_values($value);
+				$color = array();
+				$color[0] = intval(trim($tmp[0]));
+				$color[1] = intval(trim($tmp[1]));
+				$color[2] = intval(trim($tmp[2]));
+				$color[3] = intval(trim($tmp[3]));
+			}
+			elseif( is_array($value) && count($value)==3 ) {
 				$tmp = array_values($value);
 				$color = array();
 				$color[0] = intval(trim($tmp[0]));
